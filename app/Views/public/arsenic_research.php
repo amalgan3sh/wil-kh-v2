b1696e8fbf184ba65c116b175915ec54d7e686cb
ansonanroy <ansonanroy2022a@mca.ajce.in>
2024-11-23 22:46:11 +0530

    tables

--- app/Views/public/arsenic_research.php
+++ app/Views/public/arsenic_research.php
@@ -58,7 +58,170 @@
       <p class="mb-5">At WIL-KH, we prioritize accuracy, reliability, and efficiency in our arsenic testing services. Our team of experts utilizes state-of-the-art equipment and follows rigorous protocols to deliver precise and actionable results.
       Here is more about Soil Test Information :</p>
       <center><h4>WIL Soil Test Information</h4>
-      <img src="images/client/soil_package.png" alt="soil_information"></center>
+      <table border="2" cellpadding="10" cellspacing="0" style="border-collapse: collapse; border: 1px solid black;">
+        <tr>
+            <th style="border: 1px solid black;">No</th>
+            <th style="border: 1px solid black;">Parameters</th>
+            <th style="border: 1px solid black;">Method</th>
+            <th style="border: 1px solid black;">Cost per sample</th>
+            <th style="border: 1px solid black;">Remarks</th>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">1</td>
+            <td style="border: 1px solid black;">pH</td>
+            <td style="border: 1px solid black;">Electrode</td>
+            <td style="border: 1px solid black;">$5.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">2</td>
+            <td style="border: 1px solid black;">Total dissolved solids (TDS)</td>
+            <td style="border: 1px solid black;">Electrode</td>
+            <td style="border: 1px solid black;">$5.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">3</td>
+            <td style="border: 1px solid black;">Electrical Conductivity</td>
+            <td style="border: 1px solid black;">Electrode</td>
+            <td style="border: 1px solid black;">$5.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">4</td>
+            <td style="border: 1px solid black;">Salinity (ppt)</td>
+            <td style="border: 1px solid black;">Electrode</td>
+            <td style="border: 1px solid black;">$7.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">5</td>
+            <td style="border: 1px solid black;">Particles size</td>
+            <td style="border: 1px solid black;">Hydrometer</td>
+            <td style="border: 1px solid black;">$10.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">6</td>
+            <td style="border: 1px solid black;">Moisture (%)</td>
+            <td style="border: 1px solid black;">Gravimetric</td>
+            <td style="border: 1px solid black;">$5.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">7</td>
+            <td style="border: 1px solid black;">Nitrogen (Nitrate)</td>
+            <td style="border: 1px solid black;">Spectrophotometer</td>
+            <td style="border: 1px solid black;">$9.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">8</td>
+            <td style="border: 1px solid black;">Phosphorous (available)</td>
+            <td style="border: 1px solid black;">Spectrophotometer</td>
+            <td style="border: 1px solid black;">$9.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">9</td>
+            <td style="border: 1px solid black;">Potassium</td>
+            <td style="border: 1px solid black;">Spectrophotometer</td>
+            <td style="border: 1px solid black;">$10.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">10</td>
+            <td style="border: 1px solid black;">Calcium</td>
+            <td style="border: 1px solid black;">Titration</td>
+            <td style="border: 1px solid black;">$7.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">11</td>
+            <td style="border: 1px solid black;">Magnesium</td>
+            <td style="border: 1px solid black;">Spectrophotometer</td>
+            <td style="border: 1px solid black;">$7.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">12</td>
+            <td style="border: 1px solid black;">Oil and Grease</td>
+            <td style="border: 1px solid black;">Extraction</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">13</td>
+            <td style="border: 1px solid black;">Arsenic (As)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">14</td>
+            <td style="border: 1px solid black;">Chromium (Cr)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">15</td>
+            <td style="border: 1px solid black;">Iron (Fe)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">16</td>
+            <td style="border: 1px solid black;">Manganese (Mn)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">17</td>
+            <td style="border: 1px solid black;">Cadmium (Cd)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">18</td>
+            <td style="border: 1px solid black;">Lead (Pb)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">19</td>
+            <td style="border: 1px solid black;">Copper (Cu)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">20</td>
+            <td style="border: 1px solid black;">Zinc (Zn)</td>
+            <td style="border: 1px solid black;">Digestion, Spectrophotometer</td>
+            <td style="border: 1px solid black;">$30.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">21</td>
+            <td style="border: 1px solid black;">Total Nitrogen</td>
+            <td style="border: 1px solid black;">Kjeldahl</td>
+            <td style="border: 1px solid black;">$15.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black;">22</td>
+            <td style="border: 1px solid black;">Total Phosphorus</td>
+            <td style="border: 1px solid black;">Spectrophotometer</td>
+            <td style="border: 1px solid black;">$15.00</td>
+            <td style="border: 1px solid black;"></td>
+        </tr>
+    </table>
+    </center>
     </div>
   </div>
 </section>
