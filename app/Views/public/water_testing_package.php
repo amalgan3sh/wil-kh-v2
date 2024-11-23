1a8559cb4e523f9047ca39ecd4b3563adb54d04e
ansonanroy <ansonanroy2022a@mca.ajce.in>
2024-11-23 23:21:43 +0530

    color again

--- app/Views/public/water_testing_package.php
+++ app/Views/public/water_testing_package.php
@@ -56,9 +56,9 @@
             <td style="border: 1px solid black;">E. coli</td>
             <td style="border: 1px solid black;">Membrane Filtration</td>
             <td style="border: 1px solid black;">$9.00</td>
-            <td rowspan="4" style="border: 1px solid black; writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #e7e4e4;">Package 1 (Microbiological tests) = 33$</td>
-            <td rowspan="12" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #9e9a9a;">Package 3 (Basic drinking hazards) = 120$</td>
-            <td rowspan="28" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #716f6f;">Package 5 (Include aesthetic) = 205$</td>
+            <td rowspan="4" style="border: 1px solid black; writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #f0eeee;">Package 1 (Microbiological tests) = 33$</td>
+            <td rowspan="12" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #b0afaf;">Package 3 (Basic drinking hazards) = 120$</td>
+            <td rowspan="28" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #6d6d6d;">Package 5 (Include aesthetic) = 205$</td>
         </tr>
         <tr>
             <td style="border: 1px solid black;">2</td>
@@ -83,7 +83,7 @@
             <td style="border: 1px solid black;">Arsenic (As)</td>
             <td style="border: 1px solid black;">Electrode</td>
             <td style="border: 1px solid black;">$15.00</td>
-            <td rowspan="8" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #c2c0c0;">Package 2 (Chemical hazards) = 90$</td>
+            <td rowspan="8" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #dedbdb;">Package 2 (Chemical hazards) = 90$</td>
         </tr>
         <tr>
             <td style="border: 1px solid black;">6</td>
@@ -133,7 +133,7 @@
             <td style="border: 1px solid black;">Colorimetry</td>
             <td style="border: 1px solid black;">$3.00</td>
             <td rowspan="16" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle;"></td>
-            <td rowspan="16" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle;">Package 4 (Aesthetic properties) = 90$</td>
+            <td rowspan="16" style="border: 1px solid black;  writing-mode: vertical-lr; text-align: center; vertical-align: middle; background-color: #929191;">Package 4 (Aesthetic properties) = 90$</td>
             
         </tr>
         <tr>
